cdac0298b05ede64f01f1ef7a5260ce68979657f
hosein.nzd <hosein.nzd26@gmail.com>
2022-06-11 18:10:19 +0430

    Start Project

---
+++ app/src/main/java/com/hosein/nzd/studentapplication/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hosein.nzd.studentapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hosein.nzd.studentapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
